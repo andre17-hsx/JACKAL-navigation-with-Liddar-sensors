20917bf481f7ec31ba07a51296282ac401d34082
Andre Aguirre <andaapol@espol.edu.ec>
2023-09-21 20:42:33 -0500

    Project commit

---
+++ build/pckg/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/devel/.private/pckg:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/devel/.private/pckg/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/devel/.private/pckg/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/build/pckg'
+export PYTHONPATH="/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/devel/.private/pckg/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/devel/.private/pckg/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/martinelli/WidowX_XM430_Robot_Turret/catkin_ws/src/pckg:$ROS_PACKAGE_PATH"
